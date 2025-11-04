a997fb81bb8a4625dfebb47033bdef5f92d25637
Alexxus <85314303+AlexisOl@users.noreply.github.com>
2025-11-04 00:47:43 -0600

    Update CorsConfig.java

--- gateway/src/main/java/com/example/gateway/CorsConfig.java
+++ gateway/src/main/java/com/example/gateway/CorsConfig.java
@@ -8,22 +8,8 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
-@Configuration
+
 public class CorsConfig {
 
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of(
-                "http://localhost:4200",
-        ));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true);
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsWebFilter(source);
-    }
 }
